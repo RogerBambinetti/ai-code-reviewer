5cdefbe8c5b61f6f99d011bac57c183f2fbfac76
Roger <rogerbambinetti@hotmail.com>
2025-02-25 15:12:24 -0300

    invoke action

--- .github/scripts/ai-review.mjs
+++ .github/scripts/ai-review.mjs
@@ -3,7 +3,7 @@ import { Octokit } from "@octokit/rest";
 import OpenAI from "openai";
 
 
-console.log('OPENAI_API_KEY', process.env.OPENAI_API_KEY)
+console.log('OPENAI_API_KEY', process.env.OPENAI_API_KEY);
 
 const openai = new OpenAI({ apiKey: process.env.OPENAI_API_KEY });
 const octokit = new Octokit({ auth: process.env.GITHUB_TOKEN });
